2f94fd9326b3e3613ae8bde32098445d12129a33
Helder <he7d3r@gmail.com>
2012-02-14 17:57:34 +0000

    Direct imagelinks to Commons
    Based on
    https://www.mediawiki.org/w/index.php?title=Snippets/Direct_imagelinks_to_Commons&oldid=457302
    
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/Direct imagelinks to Commons.js
@@ -0,0 +1,25 @@
+/**
+ * Direct imagelinks to Commons
+ *
+ * @source: http://www.mediawiki.org/wiki/Snippets/Direct_imagelinks_to_Commons
+ * @author: [[commons:User:Krinkle]]
+ * @rev: 4
+ */
+if ( mw.config.get( 'wgNamespaceNumber', 0 ) >= 0 ) {
+        $( function() {
+                // Must be relative without "https://secure.wikimedia.org."
+                // to avoid triggering 'div#content a[href ^="https://"]' lock-icons
+                var     commonsBase = mw.config.get( 'wgServer' ) === "https://secure.wikimedia.org"
+                                ? '/wikipedia/commons/wiki/File:'
+                                : '//commons.wikimedia.org/wiki/File:',
+                        localBase = mw.util.wikiGetlink( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ),
+                        uploadBaseRe = new RegExp( '^' + $.escapeRE( '//upload.wikimedia.org/wikipedia/commons/' ) );
+ 
+                $( 'a.image' ).attr( 'href', function( i, currVal ) {
+                        if ( uploadBaseRe.test( $(this).find( 'img' ).attr( 'src' ) ) ) {
+                                return currVal.replace( localBase, commonsBase );
+                        }
+                });
+ 
+        });
+}
