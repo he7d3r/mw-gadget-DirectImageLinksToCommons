f78d6c4ca55f5bec4cf918a751d076ae61dc8b4f
Helder <he7d3r@gmail.com>
2014-09-27 20:48:58 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/DirectImageLinksToCommons.js
+++ src/DirectImageLinksToCommons.js
@@ -1,30 +1,30 @@
 /**
  * Direct imagelinks to Commons
  *
- * @source: http://www.mediawiki.org/wiki/Snippets/Direct_imagelinks_to_Commons
- * @author: [[commons:User:Krinkle]]
+ * @source: www.mediawiki.org/wiki/Snippets/Direct_imagelinks_to_Commons
+ * @author: Krinkle
  * @rev: 4
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-if ( mw.config.get( 'wgNamespaceNumber', 0 ) >= 0 ) {
-	$( function() {
-		// Must be relative without "https://secure.wikimedia.org."
-		// to avoid triggering 'div#content a[href ^="https://"]' lock-icons
-		var     commonsBase = mw.config.get( 'wgServer' ) === "https://secure.wikimedia.org"
-				? '/wikipedia/commons/wiki/File:'
-				: '//commons.wikimedia.org/wiki/File:',
-			localBase = mw.util.getUrl( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ),
-			uploadBaseRe = new RegExp( '^' + $.escapeRE( '//upload.wikimedia.org/wikipedia/commons/' ) );
-		/*jshint unused:false */
-		$( 'a.image' ).attr( 'href', function( i, currVal ) {
-			if ( uploadBaseRe.test( $(this).find( 'img' ).attr( 'src' ) ) ) {
-				return currVal.replace( localBase, commonsBase );
-			}
+	if ( mw.config.get( 'wgNamespaceNumber', 0 ) >= 0 ) {
+		$( function () {
+			// Must be relative without "https://secure.wikimedia.org."
+			// to avoid triggering 'div#content a[href ^="https://"]' lock-icons
+			var commonsBase = mw.config.get( 'wgServer' ) === 'https://secure.wikimedia.org'
+					? '/wikipedia/commons/wiki/File:'
+					: '//commons.wikimedia.org/wiki/File:',
+				localBase = mw.util.getUrl( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ),
+				uploadBaseRe = new RegExp( '^' + $.escapeRE( '//upload.wikimedia.org/wikipedia/commons/' ) );
+			/*jshint unused:false */
+			$( 'a.image' ).attr( 'href', function ( i, currVal ) {
+				if ( uploadBaseRe.test( $(this).find( 'img' ).attr( 'src' ) ) ) {
+					return currVal.replace( localBase, commonsBase );
+				}
+			});
+			/*jshint unused:true */
 		});
-		/*jshint unused:true */
-	});
-}
+	}
 
 }( mediaWiki, jQuery ) );
