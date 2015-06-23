2dcdf429b1efae69c2a6cece52587a81f958cbdc
Helder <he7d3r@gmail.com>
2015-06-23 16:58:04 -0300

    Sync with mw:Snippets/Direct imagelinks to Commons
    Copy from [mw:Snippets/Direct imagelinks to Commons](https://www.mediawiki.org/w/index.php?title=Snippets/Direct_imagelinks_to_Commons&oldid=1688652).

--- src/DirectImageLinksToCommons.js
+++ src/DirectImageLinksToCommons.js
@@ -1,30 +1,30 @@
 /**
  * Direct imagelinks to Commons
  *
- * @source: www.mediawiki.org/wiki/Snippets/Direct_imagelinks_to_Commons
- * @author: Krinkle
- * @rev: 4
+ * Required modules: mediawiki.RegExp, mediawiki.util
+ *
+ * @source www.mediawiki.org/wiki/Snippets/Direct_imagelinks_to_Commons
+ * @author Krinkle
+ * @version 2015-06-23
  */
-( function ( mw, $ ) {
-	'use strict';
-
-	if ( mw.config.get( 'wgNamespaceNumber', 0 ) >= 0 ) {
-		$( function () {
-			// Must be relative without "https://secure.wikimedia.org."
-			// to avoid triggering 'div#content a[href ^="https://"]' lock-icons
-			var commonsBase = mw.config.get( 'wgServer' ) === 'https://secure.wikimedia.org'
-					? '/wikipedia/commons/wiki/File:'
-					: '//commons.wikimedia.org/wiki/File:',
-				localBase = mw.util.getUrl( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ),
-				uploadBaseRe = new RegExp( '^' + $.escapeRE( '//upload.wikimedia.org/wikipedia/commons/' ) );
-			/*jshint unused:false */
-			$( 'a.image' ).attr( 'href', function ( i, currVal ) {
-				if ( uploadBaseRe.test( $(this).find( 'img' ).attr( 'src' ) ) ) {
-					return currVal.replace( localBase, commonsBase );
-				}
-			});
-			/*jshint unused:true */
-		});
-	}
-
-}( mediaWiki, jQuery ) );
+if ( mw.config.get( 'wgNamespaceNumber', 0 ) >= 0 ) {
+	mw.hook( 'wikipage.content' ).add( function ( $content ) {
+		var
+			uploadBaseRe = /^\/\/upload\.wikimedia\.org\/wikipedia\/commons/,
+ 
+			localBasePath = new RegExp( '^' + mw.RegExp.escape( mw.util.getUrl( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ) ) ),
+			localBaseScript = new RegExp( '^' + mw.RegExp.escape( mw.util.wikiScript() + '?title=' + mw.util.wikiUrlencode( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ) ) ),
+ 
+			commonsBasePath = '//commons.wikimedia.org/wiki/File:',
+			commonsBaseScript = '//commons.wikimedia.org/w/index.php?title=File:';
+ 		/*jshint unused:false */
+		$content.find( 'a.image' ).attr( 'href', function ( i, currVal ) {
+			if ( uploadBaseRe.test( $( this ).find( 'img' ).attr( 'src' ) ) ) {
+				return currVal
+					.replace( localBasePath, commonsBasePath )
+					.replace( localBaseScript, commonsBaseScript );
+			}
+		} );
+		/*jshint unused:true */
+	} );
+}
