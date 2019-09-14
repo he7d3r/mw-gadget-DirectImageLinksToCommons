f5c2f8cf8faf17f2d693ec6ff81848747112bc13
Helder <he7d3r@gmail.com>
2019-09-14 10:37:56 -0300

    Replace deprecated mw.RegExp.escape by mw.util.escapeRegExp
    See
    * https://phabricator.wikimedia.org/T218339
    * https://github.com/wikimedia/mediawiki/commit/8697ba835463c9413699a01293cf8f4e88ef8e35
    * https://github.com/wikimedia/mediawiki/commit/5a5abfd18a6b27f0919fe2ee391fffd09bf637df

--- src/DirectImageLinksToCommons.js
+++ src/DirectImageLinksToCommons.js
@@ -1,20 +1,20 @@
 /**
  * Direct imagelinks to Commons
  *
- * Required modules: mediawiki.RegExp, mediawiki.util
+ * Required modules: mediawiki.util
  *
  * @source www.mediawiki.org/wiki/Snippets/Direct_imagelinks_to_Commons
  * @author Krinkle
- * @version 2015-06-23
+ * @version 2019-09-14
  */
 if ( mw.config.get( 'wgNamespaceNumber', 0 ) >= 0 ) {
 	mw.hook( 'wikipage.content' ).add( function ( $content ) {
 		var
 			uploadBaseRe = /^\/\/upload\.wikimedia\.org\/wikipedia\/commons/,
- 
-			localBasePath = new RegExp( '^' + mw.RegExp.escape( mw.util.getUrl( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ) ) ),
-			localBaseScript = new RegExp( '^' + mw.RegExp.escape( mw.util.wikiScript() + '?title=' + mw.util.wikiUrlencode( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ) ) ),
- 
+
+			localBasePath = new RegExp( '^' + mw.util.escapeRegExp( mw.util.getUrl( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ) ) ),
+			localBaseScript = new RegExp( '^' + mw.util.escapeRegExp( mw.util.wikiScript() + '?title=' + mw.util.wikiUrlencode( mw.config.get( 'wgFormattedNamespaces' )['6'] + ':' ) ) ),
+
 			commonsBasePath = '//commons.wikimedia.org/wiki/File:',
 			commonsBaseScript = '//commons.wikimedia.org/w/index.php?title=File:';
  		/*jshint unused:false */
